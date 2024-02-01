12ce8dccd5b67508f22469a9e6239e734558dbab
Miles <natureuplift@protonmail.com>
2024-02-01 08:49:09 -0400

    add flow diagram

---
+++ develop/index.js
@@ -0,0 +1,7 @@
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const shapes = require('./lib/shapes.js');
+
+// includes generateMarkdown.js file
+const generateMarkdown = require('./utils/generateMarkdown')
